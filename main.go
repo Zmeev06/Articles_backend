99c4c491f663e194265b0303fbe5f20e47220b68
zero@fedora <shinyzero0@tilde.club>
2023-10-21 22:37:13 +0300

    fix cors

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
 	"web_practicum/database"
 	. "web_practicum/handlers"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	_ "github.com/mattn/go-sqlite3"
 
 	_ "web_practicum/docs"
@@ -21,6 +23,23 @@ func main() {
 		log.Fatal(err)
 	}
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		Next:             nil,
+		AllowOriginsFunc: nil,
+		AllowOrigins:     "*",
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodHead,
+			fiber.MethodPut,
+			fiber.MethodDelete,
+			fiber.MethodPatch,
+		}, ","),
+		AllowHeaders:     "*",
+		AllowCredentials: false,
+		ExposeHeaders:    "",
+		MaxAge:           0,
+	}))
 	app.Get("/swagger/*", swagger.New(swagger.Config{}))
 	api := app.Group("/api")
 	// app.Post("api/images", postImage)
