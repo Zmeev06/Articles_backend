f8fecc442fba5e34338af0eef34829e1af0cd1be
zero@fedora <shinyzero0@tilde.club>
2023-10-21 22:56:36 +0300

    set varchar type for short fields

--- models/article.go
+++ models/article.go
@@ -4,11 +4,11 @@ package models
 
 type Article struct {
 	ID              uint64 `json:"id"`
-	Title           string `json:"title"`
-	NormalisedTitle string `json:"normalised_title"`
-	Subtitle        string `json:"subtitle"`
-	Theme           string `json:"theme"`
+	Title           string `json:"title" gorm:"type:VARCHAR(300)"`
+	NormalisedTitle string `json:"normalised_title" gorm:"type:VARCHAR(300)"`
+	Subtitle        string `json:"subtitle" gorm:"type:VARCHAR(300)"`
+	Theme           string `json:"theme" gorm:"type:VARCHAR(10)"`
 	Content         string `json:"content"`
-	CreatedAt       string `json:"created_at"`
+	CreatedAt       string `json:"created_at" gorm:"type:VARCHAR(10)"`
 	TimesVisited    uint64 `json:"times_visited"`
 }
