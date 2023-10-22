987d82581db34e29a6175e26a9cc3578bc70f5a2
zero@fedora <shinyzero0@tilde.club>
2023-10-22 13:23:09 +0300

    routing: reorder endpoints

--- routing/routing.go
+++ routing/routing.go
@@ -11,10 +11,12 @@ import (
 func Setup(app *fiber.App) {
 
 	app.Get("/swagger/*", swagger.New(swagger.Config{}))
-	api := app.Group("/api")
+
 	static := app.Group("/static")
 	static.Static("/images", "./images")
 	static.Static("/qr-codes", "./qr-codes")
+
+	api := app.Group("/api")
 	api.Post("/create/article", CreateArticle)
 	api.Get("/article/:title", GetArticle)
 	api.Get("/articles", GetAllArticles)
