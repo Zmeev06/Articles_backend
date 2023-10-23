208c9be2bfcdc640220866436c01b921f8c100fe
zero@fedora <shinyzero0@tilde.club>
2023-10-23 15:57:38 +0300

    remove static to serve them with apache/nginx

--- routing/routing.go
+++ routing/routing.go
@@ -12,13 +12,12 @@ func Setup(app *fiber.App) {
 
 	app.Get("/swagger/*", swagger.New(swagger.Config{}))
 
-	static := app.Group("/static")
-	static.Static("/images", "./images")
-	static.Static("/qr-codes", "./qr-codes")
+	// static := app.Group("/static")
+	// static.Static("/images", "./images")
+	// static.Static("/qr-codes", "./qr-codes")
 
 	api := app.Group("/api")
 	api.Post("/create/article", CreateArticle)
 	api.Get("/article/:title", GetArticle)
 	api.Get("/articles", GetAllArticles)
-	app.Static("/", "./dist")
 }
